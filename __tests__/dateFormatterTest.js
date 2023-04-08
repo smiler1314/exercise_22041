3413c7c2d74c04d46cc954af10f502b144836479
smiler1314 <54764824+smiler1314@users.noreply.github.com>
2023-04-08 21:03:53 +0100

    Solve the challenge, adding unit tests

--- __tests__/dateFormatterTest.js
+++ __tests__/dateFormatterTest.js
@@ -6,3 +6,43 @@ it('when the system date and the date to format are the same day formats as "TOD
   const dateTimeToFormat = new Date(2018, December, 15, 12, 50).getTime(); //Same day different time
   expect(format(dateTimeToFormat, systemDateTime)).toBe('TODAY');
 });
+
+it('when the system date and the date to format are not the same day formats as "DD/MM/YYYY"', () => {
+  const December = 11; //js Date object month is indexed from 0
+  const January = 0; //js Date object month is indexed from 0
+  const systemDateTime = new Date(2018, December, 15, 10, 5).getTime();
+  const dateTimeToFormat1 = new Date(2018, January, 15, 12, 50).getTime();
+  expect(format(dateTimeToFormat1, systemDateTime)).toBe('15/01/2018');
+  const dateTimeToFormat2 = new Date(2019, January, 2, 12, 50).getTime();
+  expect(format(dateTimeToFormat2, systemDateTime)).toBe('02/01/2019');
+});
+
+it('when the system date and/or the date to format are not valid timestamps, nothing is returned, and errors are logged', () => {
+  const consoleSpy = jest.spyOn(console, 'log');
+  const December = 11; //js Date object month is indexed from 0
+  const January = 0; //js Date object month is indexed from 0
+  const systemDateTime = new Date(2018, December, 15, 10, 5).getTime();
+  const dateTimeToFormat = new Date(2018, January, 15, 12, 50).getTime();
+  expect(format('', systemDateTime)).toBe('');
+  expect(consoleSpy).toHaveBeenCalledWith("invalid input parameters: '', '1544868300000'");
+  expect(format(dateTimeToFormat, '')).toBe('');
+  expect(consoleSpy).toHaveBeenCalledWith("invalid input parameters: '1516020600000', ''");
+  expect(format('', '')).toBe('');
+  expect(consoleSpy).toHaveBeenCalledWith("invalid input parameters: '', ''");
+  expect(format(0, systemDateTime)).toBe('01/01/1970');
+  expect(format({}, systemDateTime)).toBe('');
+  expect(consoleSpy).toHaveBeenCalledWith("invalid input parameters: '[object Object]', '1544868300000'");
+  expect(format(8640000000000000, systemDateTime)).toBe('13/09/275760');
+  expect(format(-8640000000000001, systemDateTime)).toBe('');
+  expect(consoleSpy).toHaveBeenCalledWith("invalid input parameters: '-8640000000000001', '1544868300000'");
+  expect(format(8640000000000001, systemDateTime)).toBe('');
+  expect(consoleSpy).toHaveBeenCalledWith("invalid input parameters: '8640000000000001', '1544868300000'");
+  expect(format(0, -8640000000000001)).toBe('');
+  expect(consoleSpy).toHaveBeenCalledWith("invalid input parameters: '0', '-8640000000000001'");
+  expect(format(0, 8640000000000001)).toBe('');
+  expect(consoleSpy).toHaveBeenCalledWith("invalid input parameters: '0', '8640000000000001'");
+  expect(format(null, 8640000000000001)).toBe('');
+  expect(consoleSpy).toHaveBeenCalledWith("invalid input parameters: 'null', '8640000000000001'");
+  expect(format(undefined, 8640000000000001)).toBe('');
+  expect(consoleSpy).toHaveBeenCalledWith("invalid input parameters: 'undefined', '8640000000000001'");
+});
--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,3 +1,37 @@
 export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
-  return '';
+	const pad = (dayOrMonth) => {
+		if (dayOrMonth >= 10) {
+			return dayOrMonth;
+		} else {
+			return '0' + dayOrMonth;
+		}
+	};
+	
+	if (typeof(dateToFormatTimeMillis) != "number" ||
+		typeof(systemDateTimeMillis) != "number" ||
+		dateToFormatTimeMillis < -8640000000000000 ||
+		dateToFormatTimeMillis > 8640000000000000 ||
+		systemDateTimeMillis < -8640000000000000 ||
+		systemDateTimeMillis > 8640000000000000
+		) {
+		console.log('invalid input parameters: \'' + dateToFormatTimeMillis + '\', \'' + systemDateTimeMillis + '\'');
+		return '';
+	}
+	
+	const dateToFormat = new Date(dateToFormatTimeMillis);
+	const systemDate = new Date(systemDateTimeMillis);
+	
+	const dd = dateToFormat.getDate();
+	const dm = dateToFormat.getMonth() + 1;
+	const dy = dateToFormat.getFullYear();
+	
+	const sd = systemDate.getDate();
+	const sm = systemDate.getMonth() + 1;
+	const sy = systemDate.getFullYear();
+	
+	if (dd === sd && dm === sm && dy === sy) {
+		return 'TODAY';
+	} else {
+		return pad(dd) + '/' + pad(dm) + '/' + dy;
+	}
 };
